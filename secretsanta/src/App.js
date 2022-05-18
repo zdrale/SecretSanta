381bc6b319f1aa79d7fc47dad52163c2a68b2dd1
zdrale <nikolazd22@outlook.com>
2022-05-18 11:46:54 +0200

    Installed React 18.1.0

---
+++ secretsanta/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import React from 'react';
+
+function App() {
+  console.log(React.version);
+  return (
+    <div className="App">
+      asd
+    </div>
+  );
+}
+
+export default App;
