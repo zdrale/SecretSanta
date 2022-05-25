c62c93f5454270cc628856d7dcb0575e42c1c931
zdrale <nikolazd22@outlook.com>
2022-05-25 09:08:32 +0200

    Added logic for making pairs

--- secretsanta/src/App.js
+++ secretsanta/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Header from './components/header/Header';
 import AddNewEmployee from './components/add-new-employee/AddNewEmployee';
-import EmployeeList from './components/employee-list/EmployeeList';
 
 function App() {
   return (
